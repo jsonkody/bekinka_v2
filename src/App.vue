dbfa377fe6e74b1ad3f76bb32c1a9894a35a7b8a
JsonKody <daniel8cech@gmail.com>
2025-02-19 22:53:07 +0100

    saved & pushed by npm

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,67 @@ import TheMenu from '@/components/TheMenu.vue'
 </script>
 
 <template>
-  <header>
-    <TheMenu />
-  </header>
+  <div class="flex relative flex-col min-h-screen justify-between">
+    <div>
+      <ScrollToTop />
 
-  <transition name="fade" mode="out-in">
-    <RouterView />
-  </transition>
+      <div class="head_position absolute z-10">
+        <img width="283" height="430" src="/images/head.webp" alt="head" />
+      </div>
+
+      <TheMenu />
+    </div>
+    <main class="grow relative px-2 py-4 md:px-8">
+      <div
+        class="floating_animation pointer-events-none inline-block absolute right-0 -mt-6 mr-6 md:mr-32 lg:mr-60"
+      >
+        <Image
+          alt="bekinka japanese"
+          src="/images/bekinka_jpn.webp"
+          priority="{true}"
+          width="{480}"
+          height="{140}"
+        />
+      </div>
+      <div
+        class="z-10 border border-purple-400 border-opacity-30 container content mx-auto pb-20 px-2 pt-2 md:pt-6 md:p-5 md:pb-20 relative"
+      >
+        <transition name="fade" mode="out-in">
+          <RouterView />
+        </transition>
+      </div>
+    </main>
+
+    <footer>
+      <TheFooter />
+    </footer>
+  </div>
 </template>
 
 <style>
 :root {
   scroll-behavior: smooth;
   color: white;
+}
+
+.head_position {
+  display: none;
+  pointer-events: none;
+}
+
+@media (min-width: 639px) {
+  .head_position {
+    display: none;
+  }
+}
+
+@media (min-width: 1800px) {
+  .head_position {
+    top: -4rem;
+    left: 18rem;
+    display: block;
+    transform: rotate(-10deg);
+  }
 }
 
 .fade-enter-active,
