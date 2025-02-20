c269c5907887144241e6d28a20dfecfaf910f520
JsonKody <daniel8cech@gmail.com>
2025-02-20 15:52:55 +0100

    saved & pushed by npm

--- src/components/SocialComponent.vue
+++ src/components/SocialComponent.vue
@@ -1,20 +1,19 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="rd social-block m-3 md:m-0 md:mr-6">
-    <div class="flex flex-row flex-wrap">
+    <!-- <div class="flex flex-row flex-wrap">
       <a
         class="social-link"
         rel="noreferrer"
         target="_blank"
         href="https://www.instagram.com/bekinka__/"
       >
-        <!-- <IconBadge4k stroke="{2}" /> -->
+
       </a>
 
       <a class="social-link" rel="noreferrer" target="_blank" href="https://twitch.tv/bekinka">
-        <!-- <n-icon size="24" :component="LogoTwitch" title="Twitch" /> -->adad
+
       </a>
 
       <a
@@ -23,7 +22,7 @@
         target="_blank"
         href="https://discord.com/invite/JB49XFU"
       >
-        <!-- <n-icon size="24" :component="LogoDiscord" title="Discord" /> -->
+
       </a>
 
       <a
@@ -32,8 +31,9 @@
         target="_blank"
         href="https://www.youtube.com/channel/UCJM8kUylpahRnHHFh3-z7tg/videos"
       >
-        <!-- <n-icon size="24" :component="LogoYoutube" title="Youtube" /> -->
+
       </a>
-    </div>
+    </div> -->
+    social xyz
   </div>
 </template>
