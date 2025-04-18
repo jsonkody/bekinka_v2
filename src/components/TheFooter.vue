77cb5fb01ab1a4a6d239713d35d09199369b26c9
jsonkody <daniel8cech@gmail.com>
2025-04-18 13:01:34 +0200

    saved & pushed by npm

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -9,11 +9,13 @@
       alt="head"
       class="absolute bottom-0 z-10"
     />
-    <span
+    <a
+    target="_blank"
+    href="https://jsonkody.cz"
       v-pop="`Beky brácha, radící Radek a pořádnej web-dev ( ͡° ͜ʖ ͡°)`"
       class="relative z-20 font-pixel p-2 sm:px-8 py-2 rounded-xl text-xl sm:text-4xl kody font-semibold text-pink-300"
     >
       <span class="hidden sm:inline">~ </span>JsonKody<span class="hidden sm:inline"> ~</span>
-    </span>
+    </a>
   </div>
 </template>
