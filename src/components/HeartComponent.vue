d7f32570084525ae55fe911b66e0f7a2debfb91e
jsonkody <daniel8cech@gmail.com>
2025-07-18 23:47:42 +0200

    saved and pushed by script

--- src/components/HeartComponent.vue
+++ src/components/HeartComponent.vue
@@ -2,47 +2,47 @@
 
 <template>
   <div class="my-5 flex justify-center">
-    <div class="w-48 flex justify-between bg-purple-200/10 rounded-xl p-3">
-      <img
-        width="21"
-        height="21"
-        class="inline"
-        src="/images/heart_green.webp"
-        alt="dobrodružné srdce"
-        v-pop:bottom="'dobrodružné srdce'"
-      />
-      <img
-        width="21"
-        height="21"
-        class="inline"
-        src="/images/heart_cyan.webp"
-        alt="diamantové srdce"
-        v-pop:bottom="'diamantové srdce'"
-      />
-      <img
-        width="21"
-        height="21"
-        class="inline"
-        src="/images/heart_purple.webp"
-        alt="statečné srdce"
-        v-pop:bottom="'statečné srdce'"
-      />
-      <img
-        width="21"
-        height="21"
-        class="inline"
-        src="/images/heart_red.webp"
-        alt="milující srdce"
-        v-pop:bottom="'milující srdce'"
-      />
-      <img
-        width="21"
-        height="21"
-        class="inline"
-        src="/images/heart_gold.webp"
-        alt="zlaté srdce"
-        v-pop:bottom="'zlaté srdce'"
-      />
+    <!-- <div class="w-48 flex justify-between bg-purple-200/10 rounded-xl p-3"> -->
+    <div class="p-3 flex bg-purple-200/10 rounded-xl">
+      <div v-pop:bottom="'dobrodružné srdce'">
+        <img
+          width="21"
+          height="21"
+          class="mx-2"
+          src="/images/heart_green.webp"
+          alt="dobrodružné srdce"
+        />
+      </div>
+      <div v-pop:bottom="'diamantové srdce'">
+        <img
+          width="21"
+          height="21"
+          class="mx-2"
+          src="/images/heart_cyan.webp"
+          alt="diamantové srdce"
+        />
+      </div>
+      <div v-pop:bottom="'statečné srdce'">
+        <img
+          width="21"
+          height="21"
+          class="mx-2"
+          src="/images/heart_purple.webp"
+          alt="statečné srdce"
+        />
+      </div>
+      <div v-pop:bottom="'milující srdce'">
+        <img
+          width="21"
+          height="21"
+          class="mx-2"
+          src="/images/heart_red.webp"
+          alt="milující srdce"
+        />
+      </div>
+      <div v-pop:bottom="'zlaté srdce'">
+        <img width="21" height="21" class="mx-2" src="/images/heart_gold.webp" alt="zlaté srdce" />
+      </div>
     </div>
   </div>
 </template>
