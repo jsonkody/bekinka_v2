2a30b704580d3fa6504999751844242a6baaf4dd
jsonkody <daniel8cech@gmail.com>
2026-03-06 10:17:29 +0100

    saved by script

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -18,42 +17,8 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'terser',
     sourcemap: false,
     cssCodeSplit: true,
-    reportCompressedSize: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Advanced chunk splitting strategy
-          if (id.includes('node_modules')) {
-            // priradí knihovny do samostatných chunku podle názvu balícku
-            const modules = [
-              'vue',
-              'vue-router',
-              'pinia',
-              'lodash',
-              'axios',
-              '@floating-ui/dom',
-              // a další knihovny, které by mohly tvorit významné cásti vašeho kódu
-            ]
 
-            const chunkName = modules.find((module) => id.includes(module))
-            return chunkName ? `vendor-${chunkName}` : 'vendor-others'
-          }
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        pure_funcs: ['console.debug'], // Keep warnings/errors
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['vue', 'vue-router', 'pinia',],
   },
 })
